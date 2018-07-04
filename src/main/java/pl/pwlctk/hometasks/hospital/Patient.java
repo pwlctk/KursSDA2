bb04d447fb104767e2d3b73033a33b9944de4d74
Paweł Citko <pwlctk@gmail.com>
2018-07-05 00:25:04 +0200

    Task: SOR Hospital

---
+++ src/main/java/pl/pwlctk/hometasks/hospital/Patient.java
@@ -0,0 +1,33 @@
+package pl.pwlctk.hometasks.hospital;
+
+public class Patient {
+    private String name;
+    private String lastName;
+    private String ailment;
+    private int priority;
+
+
+    Patient(String name, String lastName, String ailment, int priority) {
+        this.name = name;
+        this.lastName = lastName;
+        this.ailment = ailment;
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return name + ";" + lastName + ";" + ailment + ";" + priority;
+    }
+
+    int getPriority() {
+        return priority;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    String getAilment() {
+        return ailment;
+    }
+}
---
+++ src/main/java/pl/pwlctk/hometasks/hospital/PatientPriority.java
@@ -0,0 +1,10 @@
+package pl.pwlctk.hometasks.hospital;
+
+import java.util.Comparator;
+
+public class PatientPriority implements Comparator<Patient> {
+    @Override
+    public int compare(Patient o1, Patient o2) {
+        return Integer.compare(o2.getPriority(), o1.getPriority());
+    }
+}
